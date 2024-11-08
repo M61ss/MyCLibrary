531723e3544fa5591b9e724e91611a553e0e1e95
Mattia Massarenti <mattia.mas2003@gmail.com>
2024-11-08 23:03:52 +0100

    update

--- MyCLibrary/main.c
+++ MyCLibrary/main.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <ctype.h>
 
 int main(void) {
-	Box("Ehilà!", 0); NEWLINE;
+	Box("Ehilï¿½!", 0); NEWLINE;
 	LinePlus((int)strlen("ssssssssssss")); NEWLINE;
 	Piramid("Error", 0, false); NEWLINE;
 	Parallelogram("ssssssss", 3, true); NEWLINE;
-	Piramid("A testa in giù", 5, true); NEWLINE;
+	Piramid("A testa in giï¿½", 5, true); NEWLINE;
 	Box("I'm the fastest reader in the world", 4); NEWLINE;
 	Arrow(3, 0); NEWLINE;
 	Arrow(0, 1); NEWLINE;
--- MyCLibrary/main.h
+++ MyCLibrary/main.h
@@ -1,8 +1,11 @@
 // header per testare le varie librerie con il main
-#pragma once
+#ifndef MAIN_H
+  #define MAIN_H
 
 #include "tools.h"
 #include "string_functions.h"
 #include "math_functions.h"
 #include "graphics.h"
 #include "common.h"
+
+#endif // MAIN_H
